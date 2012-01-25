301c708eb39d18147e5988876ebc9e411b0b16e4
Alan Aguiar <alanjas@hotmail.com>
2012-01-25 06:27:24 -0200

    fix minimal problem

--- timelinelib/wxgui/components/cattree.py
+++ timelinelib/wxgui/components/cattree.py
@@ -233,7 +233,7 @@ def delete_category(parent_ctrl, db, cat, fn_handle_db_error):
                            "belong to a category.") % cat.name
     else:
         update_warning = _("Events belonging to " + cat.name + _(" will now belong to ") + cat.parent.name
-    question = "%s\n\n%s" % (delete_warning, update_warning)
+    question =  delete_warning + '\n\n' + update_warning
     if _ask_question(question, parent_ctrl) == wx.YES:
         try:
             db.delete_category(cat)
